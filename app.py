8916e8842b4985e35510a7cd350cd5da13dfc3b9
Soham Kulkarni <soham.kulkarni@csu.fullerton.edu>
2023-04-08 23:16:04 -0700

    http errors

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from werkzeug.exceptions import HTTPException
 from werkzeug.utils import secure_filename 
 import pymysql
 import re
+import json
 from flask_cors import CORS
 import os
 app = Flask(__name__)
@@ -23,7 +25,7 @@ ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 conn = pymysql.connect(
         host='localhost',
         user='root', 
-        password = "adminadmin",
+        password = "Soham@12345",
         db='449_db',
 		cursorclass=pymysql.cursors.DictCursor
         )
@@ -33,15 +35,37 @@ app.config['SECRET_KEY'] = 'super-secret'
 
 jwt = JWTManager(app)
 
+# @app.errorhandler(HTTPException)
+# def handle_exception(e):
+#     """Return JSON instead of HTML for HTTP errors."""
+#     # start with the correct headers and status code from the error
+#     response = e.get_response()
+#     # replace the body with JSON
+#     response.data = json.dumps({
+#         "code": e.code,
+#         "name": e.name,
+#         "description": e.description,
+#     })
+#     response.content_type = "application/json"
+#     return response
+
+
 @app.route("/login", methods=["POST"])
 def login():
     username = request.json.get("username", None)
     password = request.json.get("password", None)
-    if username != "aniket" or password != "aniket":
-        return jsonify({"msg": "Bad username or password"}), 401
-
-    access_token = create_access_token(identity=username)
-    return jsonify(access_token=access_token)
+    if username and password:
+        cur.execute('SELECT * FROM users WHERE username = % s AND password = % s', (username, password))
+        conn.commit()
+        account = cur.fetchone()
+        if account:
+            access_token = create_access_token(identity=username)
+            return jsonify(access_token=access_token)
+        else:
+            return jsonify({'error': 'Unauthorized access'}), 401
+    else:
+        return jsonify({'error': 'Bad request'}), 400
+    
 
 @app.route("/protected", methods=["GET"])
 @jwt_required()
@@ -77,6 +101,19 @@ def upload():
         
     return "File saved and uploaded"
 
+
+@app.errorhandler(400)
+def handle_Error_400(e):
+    return jsonify({'error': str(e)}), 400
+
+@app.errorhandler(404)
+def handle_Error_404(e):
+    return jsonify({'error': str(e)}), 404
+
+
+@app.errorhandler(401)
+def handle_Error_401(e):
+    return jsonify({'error': str(e)}), 401
 
 
 if __name__ == "__main__":
